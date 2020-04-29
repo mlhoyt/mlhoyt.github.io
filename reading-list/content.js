b5192d5b3ef3f02383fa3e67dac78ba50a6566b5
Mike Hoyt <mhoyt@shujinko.io>
2020-04-28 20:56:08 -0700

    move content list from action.js to content.js

---
+++ reading-list/content.js
@@ -0,0 +1,17 @@
+content = [
+  new ContentEntry(1518235200, "The Twelve-Factor App"),
+  new ContentEntry(1516970676, "theagileadmin: Learning Kubernetes and K8s Native Tools"),
+  new ContentEntry(1516970607, "theagileadmin: CNCF and K8s 101"),
+]
+// let tags = {
+//   Keys: function() {
+//     return [
+//       <TAG>,
+//       ...
+//     ]
+//   },
+//   <TAG>: [
+//     <ENTRY-ID>,
+//     ...
+//   ],
+// }
